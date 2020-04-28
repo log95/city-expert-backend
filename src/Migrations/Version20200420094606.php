91a4b350d43f9d6dcac75d03d017017df6e68836
Andrey Frolov <andrei.frolov@maximumtest.ru>
2020-04-28 15:05:22 +0300

    Fix migrations

--- src/Migrations/Version20200420094606.php
+++ src/Migrations/Version20200420094606.php
@@ -22,7 +22,6 @@ final class Version20200420094606 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('DROP SEQUENCE users_id_seq CASCADE');
         $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
@@ -35,7 +34,6 @@ final class Version20200420094606 extends AbstractMigration
 
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
-        $this->addSql('CREATE SEQUENCE users_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('DROP TABLE "user"');
     }
 }
--- src/Migrations/Version20200423101552.php
+++ src/Migrations/Version20200423101552.php
@@ -22,10 +22,8 @@ final class Version20200423101552 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('DROP SEQUENCE movie_id_seq CASCADE');
         $this->addSql('CREATE SEQUENCE test_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE test (id INT NOT NULL, question VARCHAR(255) DEFAULT NULL, answer VARCHAR(255) NOT NULL, hints JSON DEFAULT NULL, image VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('DROP TABLE movie');
     }
 
     public function down(Schema $schema) : void
@@ -35,8 +33,6 @@ final class Version20200423101552 extends AbstractMigration
 
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE test_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE movie_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE movie (id INT NOT NULL, name VARCHAR(100) NOT NULL, description TEXT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('DROP TABLE test');
     }
 }
